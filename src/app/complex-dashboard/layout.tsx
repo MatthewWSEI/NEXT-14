1aa7567f4eca7071b078bc22b8a6c55248d760c2
Mateusz Dynur <mateusz.dynur@microsoft.wsei.edu.pl>
2024-02-24 17:56:18 +0100

    add more files

---
+++ src/app/complex-dashboard/layout.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+// import UserAnalytics from "@/components/UserAnalytics";
+// import RevenueMetrics from "@/components/RevenueMetrics";
+// import Notification from "@/components/Notification";
+const DashboardLayout = ({
+  children,
+  users,
+  revenue,
+  notifications,
+}: {
+  children: React.ReactNode;
+  users: React.ReactNode;
+  revenue: React.ReactNode;
+  notifications: React.ReactNode;
+}) => {
+  return (
+    <>
+      <div>{children}</div>
+      <div style={{ display: "flex" }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div>{users}</div>
+          <div>{revenue}</div>
+        </div>
+        <div style={{ display: "flex", flex: 1 }}>{notifications}</div>
+      </div>
+
+      {/* <UserAnalytics />
+      <RevenueMetrics />
+      <Notification /> */}
+    </>
+  );
+};
+
+export default DashboardLayout;
